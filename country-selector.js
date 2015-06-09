5255bedeed07331a5705d33bd57a1361f47ad26c
Duy Tran <dangduy2910@gmail.com>
2015-06-09 16:19:09 +0700

    Create country-selector.js

---
+++ country-selector.js
@@ -0,0 +1,61 @@
+angular.module('directive.countrySelector', [])
+
+.directive('countrySelector', function ($document, $timeout, CountryCodes) {
+    return {
+        scope: {
+            ngModel: '=',
+            onSelected: '&',
+        },
+        replace: true,
+        require: '^ngModel',
+        templateUrl: 'directives/templates/country-selector.tpl.html',
+        link: function (scope, element, attrs, ngModelCtrl) {
+            scope.isOpen = false;
+            scope.CountryCodes = CountryCodes;
+
+            function onDocumentClick(e) {
+                if (scope.isOpen && !element[0].contains(e.target)) {
+                    scope.isOpen = false;
+                }
+                scope.$digest();
+            }
+
+            // See Click everywhere but here event http://stackoverflow.com/questions/12931369
+            $document.on('click', onDocumentClick);
+
+            scope.$on('$destroy', function () {
+                $document.off('click', onDocumentClick);
+            });
+
+            scope.$watch('isOpen', function () {
+                if (scope.isOpen) {
+                    $(element).find('.searchbox input').focus();
+                    var selectedTag = $(element).find('li.selected')[0];
+                    if (selectedTag) {
+                        scrollToSelected($(element.find('ul')[0]), $(selectedTag));
+                    }
+                }
+            });
+
+            scope.showDropDown = function () {
+                scope.isOpen = true;
+            };
+
+            scope.chooseCountry = function (countryCode) {
+                scope.selectedCountry = countryCode;
+                scope.isOpen = false;
+                scope.search = '';
+                ngModelCtrl.$setViewValue(countryCode.isoCode);
+                scope.onSelected(countryCode);
+            };
+        }
+    };
+
+    function scrollToSelected($container, $selected) {
+        $container.scrollTop(0);
+        var selectedOffset = $selected.offset().top;
+        var selectedHeight = $selected.outerHeight(false);
+        var containerOffset = $container.offset().top;
+        $container.scrollTop(selectedOffset - containerOffset - selectedHeight * 2);
+    }
+});
